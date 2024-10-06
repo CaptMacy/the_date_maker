fd2a1996ee5fd8556b1370cf7595ed1deb2bd387
CaptMacy <jakku.macey@gmail.com>
2024-10-06 15:37:20 +0100

    view buttons added

--- the_date_maker/components/header/header.js
+++ the_date_maker/components/header/header.js
@@ -1,15 +1,23 @@
 import './header.css';
 
-// export function loadHeader() {
-//     return fetch('components/header/header.html')
-//         .then(response => response.text())
-//         .then(html => {
-//             console.log(111);
+export function loadHeader() {
+    return fetch('components/header/header.html')
+        .then(response => response.text())
+        .then(html => {
+            console.log(111);
             
-//             console.log(html);
+            console.log(html);
 
-//             const header = document.getElementById('header')
-//             header.innerHTML = html            
-//         })
-//         .catch(err => {console.log('failed to load header', err)});
-// }
+            const header = document.getElementById('header')
+            header.innerHTML = html            
+        })
+        .catch(err => {console.log('failed to load header', err)});
+}
+
+const buildHeader = async() => {
+    await loadHeader();
+
+    const header = document.querySelector('.header')
+}
+
+buildHeader();
