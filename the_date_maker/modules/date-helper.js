6b7aa41f42264486ece77e977130704ee2a971bc
CaptMacy <jakku.macey@gmail.com>
2024-11-16 16:50:37 +0000

    hours added

--- the_date_maker/modules/date-helper.js
+++ the_date_maker/modules/date-helper.js
@@ -75,6 +75,8 @@ const daysInWeek = (sunday) => {
 
         if(day.getDate() == currentDay) dayElement.classList.add('currentDay');
     }
+
+    hours();
 }
 
 const handleDateDisplayChange = (event) => {
@@ -120,4 +122,17 @@ const handleMonthChange = (event) => {
     updateMonthDisplay();
     
     daysInMonth(currentDate.getFullYear(), currentDate.getMonth() + 1, false);
+}
+
+const hours = () => {
+    const hourSidebar = document.getElementById('calendar-hour-sidebar');
+
+    const hourList = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+
+    hourList.forEach(hour => {
+        const hourElement = document.createElement('div');
+        hourElement.innerText = hour;
+
+        hourSidebar.appendChild(hourElement);
+    });
 }
